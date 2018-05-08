8508ea8baba798b2b0b88eaabc871eccd23a701e
Andrew Romanov <scalli-k-be@ya.ru>
2018-05-08 11:18:59 +0700

    Initial Commit

---
+++ RemoveOldTimeMashine/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RemoveOldTimeMashine
+//
+//  Created by Andrew Romanov on 08/05/2018.
+//  Copyright © 2018 Andrew Romanov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
