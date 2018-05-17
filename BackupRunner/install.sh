ba40d51bf7fbd684cc3a5dac042ca47dcd6ca23e
Andrew Romanov <scalli-k-be@ya.ru>
2018-05-17 10:44:54 +0700

    added commented crontab

--- BackupRunner/install.sh
+++ BackupRunner/install.sh
@@ -11,3 +11,8 @@ DESTINATION=$SCRIPT_DIRECTORY/RunBackup.sh
 cp $DESTINATION ~/autorunScripts/timeMashine/RunBackup.sh
 echo "installed to ${DESTINATION}"
 
+#CronTab file:
+#SHELL=/bin/bash
+#PATH=/opt/local/bin:/opt/local/sbin:/Library/Frameworks/Python.framework/Versions/3.5/bin:/Users/andrew/bin:/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Frameworks/Mono.framework/Commands/
+#
+#5 * * * *  bash ~/autorunScripts/timeMashine/RunBackup.sh
